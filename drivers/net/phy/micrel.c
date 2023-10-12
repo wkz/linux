12ea9e9ffc9684b48dd95f9ccdc5856ed31e572a
Horatiu Vultur <horatiu.vultur@microchip.com>
2023-10-12 08:17:52 +0200

    phy: micrel: lan8814: Allow to have external clock source

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -3738,6 +3738,57 @@ static int lan8814_config_intr(struct phy_device *phydev)
 	return err;
 }
 
+#define LAN8814_PTP_REF_CLK_CFG			514
+#define LAN8814_PTP_REF_CLK_CFG_SOURCE_M	GENMASK(12, 10)
+#define LAN8814_PTP_REF_CLK_CFG_SOURCE(x)	((x)<<10)
+
+#define LAN8814_PTP_PLL_DIV			323
+#define LAN8814_PTP_PLL_DIV_DIVR_M		GENMASK(14, 10)
+#define LAN8814_PTP_PLL_DIV_DIVR(x)		((x)<<10)
+#define LAN8841_PTP_PLL_DIV_DIVF_M		GENMASK(9, 3)
+#define LAN8841_PTP_PLL_DIV_DIVF(x)		((x)<<3)
+
+static int lan8814_ptp_pll_init(struct phy_device *phydev)
+{
+	struct kszphy_priv *priv = phydev->priv;
+	struct kszphy_ptp_priv *ptp_priv = &priv->ptp_priv;
+	u32 divr, divf;
+	u32 val;
+
+	switch (ptp_priv->ptp_clock_freq) {
+	case 10000000:
+		divr = 0x00;
+		divf = 0x31;
+		break;
+	case 25000000:
+		divr = 0x00;
+		divf = 0x13;
+		break;
+	case 125000000:
+		divr = 0x04;
+		divf = 0x13;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	/* Configure to use external clock source */
+	val = lanphy_read_page_reg(phydev, 4, LAN8814_PTP_REF_CLK_CFG);
+	val &= ~LAN8814_PTP_REF_CLK_CFG_SOURCE_M;
+	val |= LAN8814_PTP_REF_CLK_CFG_SOURCE(2);
+	lanphy_write_page_reg(phydev, 4, LAN8814_PTP_REF_CLK_CFG, val);
+
+	/* Configure to generate 250MHz use by the timestamping unit */
+	val = lanphy_read_page_reg(phydev, 4, LAN8814_PTP_PLL_DIV);
+	val &= ~LAN8814_PTP_PLL_DIV_DIVR_M;
+	val &= ~LAN8841_PTP_PLL_DIV_DIVF_M;
+	val |= LAN8814_PTP_PLL_DIV_DIVR(divr);
+	val |= LAN8841_PTP_PLL_DIV_DIVF(divf);
+	lanphy_write_page_reg(phydev, 4, LAN8814_PTP_PLL_DIV, val);
+
+	return 0;
+}
+
 static void lan8814_ptp_init(struct phy_device *phydev)
 {
 	struct kszphy_priv *priv = phydev->priv;
@@ -3793,6 +3844,8 @@ static void lan8814_ptp_init(struct phy_device *phydev)
 static int lan8814_ptp_probe_once(struct phy_device *phydev)
 {
 	struct lan8814_shared_priv *shared = phydev->shared->priv;
+	struct kszphy_priv *priv = phydev->priv;
+	struct kszphy_ptp_priv *ptp_priv = &priv->ptp_priv;
 
 	/* Initialise shared lock for clock*/
 	mutex_init(&shared->shared_lock);
@@ -3855,6 +3908,17 @@ static int lan8814_ptp_probe_once(struct phy_device *phydev)
 	lanphy_write_page_reg(phydev, 4, PTP_OPERATING_MODE,
 			      PTP_OPERATING_MODE_STANDALONE_);
 	lan8814_gpio_init(shared);
+
+	if (!of_property_read_u32(phydev->mdio.dev.of_node,
+				 "microchip,1588-clock-freq",
+				 &ptp_priv->ptp_clock_freq)) {
+		int err;
+
+		err = lan8814_ptp_pll_init(phydev);
+		if (err)
+			return err;
+
+	}
 
 	return 0;
 }
